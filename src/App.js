006e29bf5a15aa69801fcd57d73abe38425b1cfc
Caitlin RS <caitlinrobertsonswift@gmail.com>
2024-07-11 13:56:33 +0200

    trying to fix 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ContactPage } from "./pages/ContactPage";
 import { EduPage } from "./pages/EduPage";
 import { HomePage } from './pages/HomePage';
